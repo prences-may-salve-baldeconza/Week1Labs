99e112e678f5143a58881cbeb123a7a885668c42
May <p.baldeconza.cs@gmail.com>
2026-08-28 20:19:11 +0800

    Lab 5: Navigation between Welcome and Addtask screens

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,5 +1,5 @@
-import { View, Text, StyleSheet } from 'react-native';
-export default function WelcomeScreen() {
+import { View, Text, StyleSheet, Button } from 'react-native';
+export default function WelcomeScreen({ navigation }) {
  return (
  <View style={styles.container}>
  <View style={styles.header}>
@@ -8,11 +8,14 @@ export default function WelcomeScreen() {
  <Text style={styles.subtitle}>Built by you, one lab at a time</Text>
  </View>
  <View style={styles.footer}>
- <Text style={styles.footerText}>Lab 3: Flexbox Layout</Text>
+ <Button
+ title="May's Tasks ->"
+ onPress={() => navigation.navigate('AddTask')}
+ />
  </View>
  </View>
  );
-}
+ }
 const styles = StyleSheet.create({
  container: { flex: 1, backgroundColor: '#1B2A4A', justifyContent: 'space-between'
 },
@@ -21,5 +24,4 @@ const styles = StyleSheet.create({
  title: { fontSize: 28, fontWeight: 'bold', color: '#FFFFFF' },
  subtitle: { fontSize: 14, color: '#C7D2E8', marginTop: 6 },
  footer: { paddingBottom: 40, alignItems: 'center' },
- footerText: { color: '#9FB0D0', fontSize: 12 },
 });
